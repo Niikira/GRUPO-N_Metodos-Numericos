fab58ca385747203a5c4c2cae40a59228ad09417
Niikira <nicolascelestrino@gmail.com>
2026-03-21 17:26:00 -0300

    implementação do cálculo aproximado da integral

--- IntegralNumerica.py
+++ IntegralNumerica.py
@@ -8,7 +8,6 @@ from sympy import symbols, sympify
 
 #a ser feito:
 #lógica de cálculo de integral com loop de somatória
-#construção da tabela com x e f(x) dentro do loop
 #todas as saídas especificadas
 #erro de truncamento (se der tempo)
 
@@ -22,27 +21,33 @@ from sympy import symbols, sympify
 # Feito a entrada de dados
 # Refatoração do código para melhor legibilidade (clean code)
 
+#Niikira:
+#descarte da função "area_trapezio" pois não será mais necessária (comentada)
+#construção da tabela (x  f(x))
+#calculo aproximado da integral
+
 
 # Entrada de dados
-numeroDeTrapezios = input("Quantos trapézios você gostaria? ")
-numeroDeCasasDecimais = input("Quantas casas decimais você gostaria? ")
-passo = input("Qual a altura dos trapezios? ")
 expressao = input("Qual é a função? ")
 intervalo = [input("Qual o começo do intervalo? "), input("Qual o fim do intervalo? ")]
-print(numeroDeTrapezios)
-print(numeroDeCasasDecimais)
-print(passo)
+numeroDeTrapezios = int(input("Quantos trapézios você gostaria? "))
+numeroDeCasasDecimais = int(input("Quantas casas decimais você gostaria? "))
+#passo = input("Qual a altura dos trapezios? ") -> o passo não é recebido como entrada
 print(expressao)
 print (intervalo)
+print(numeroDeTrapezios)
+print(numeroDeCasasDecimais)
+#print(passo)
 # Definições de métodos
 
 """
     Precisa: Calcular a base menor, calcular a base maior, a altura antes de chamar esse método
-"""
+
 def area_trapezio(baseMenor, baseMaior, altura):
     areaTrapezio = ((baseMaior + baseMenor)*altura)/2 
     
     return areaTrapezio
+"""
 
 def erro_arredondamento(numeroDeTrapezios, numeroDeCasasDecimais, passo): 
     ErroArredondamento = numeroDeTrapezios * ((10 ** (-numeroDeCasasDecimais))/2) * passo 
@@ -53,7 +58,22 @@ def erro_arredondamento(numeroDeTrapezios, numeroDeCasasDecimais, passo):
 x = symbols('x') #define x como variavel
 func = sympify(expressao) #conversão da string de entrada para função matemática
 print(func)
-inicio = intervalo[0]
-fim = intervalo[1]
-# altura = (fim-inicio)/numeroDeTrapezios -> não é necessário pois o passo já é a altura.
-# Se quiser mesmo calcular a altura precisa fazer um parse pois o input recebe string.
+inicio = int(intervalo[0])
+fim = int(intervalo[1])
+passo = (fim-inicio)/numeroDeTrapezios
+dados = {"x": [], "f(x)": []}
+
+#loop de preenchimento da tabela "x  f(x)"
+for i in range(inicio, fim+1, passo):
+    dados["x"].append(i)
+    dados["f(x)"].append(func.subs(x, i))
+#criacao da tabela
+tabela = pd.DataFrame(dados)
+#cópia da coluna "f(x)" para calculo da integral
+colunafx = tabela["f(x)"].copy()
+#divisão do primeiro e do ultimo valor por 2 para a somatoria das areas do trapezio
+colunafx.iloc[0] = colunafx.iloc[0]/2
+colunafx.iloc[-1] = colunafx.iloc[-1]/2
+#somatória das areas
+integral = colunafx.sum()
+
